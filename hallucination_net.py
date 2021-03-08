f3b65ee29e22e14c423780a9a1187c132556a08d
kashyappiyush1998 <kashyappiyush1998@gmail.com>
2021-03-08 17:14:23 +0530

    test

--- hallucination_net.py
+++ hallucination_net.py
@@ -40,7 +40,7 @@ import tensorflow as tf
 import tensorlayer as tl
 import numpy as np
 
-tf.compat.v1.disable_eager_execution()
+tf.disable_eager_execution()
 
 # The HDR reconstruction autoencoder fully convolutional neural network
 def model(x, batch_size=1, is_training=False):
